4f26d665b8210302e5475575aaf4342a55487c22
Simon Landry <simon.landry@rumandcode.io>
2025-10-15 17:23:42 -0400

    Rename to Hyftar-Clan-Events

--- src/test/java/com/HyftarClanEvents/HyftarClanEventsPluginTest.java
+++ src/test/java/com/HyftarClanEvents/HyftarClanEventsPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.HyftarClanEvents;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class HyftarClanEventsPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(HyftarClanEventsPlugin.class);
 		RuneLite.main(args);
 	}
 }
